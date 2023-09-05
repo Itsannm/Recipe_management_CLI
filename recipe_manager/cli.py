f34ef517d21e172b4fad326178ea6771d5d9b7df
Itsannm <ann.mwanzia@student.moringaschool.com>
2023-09-05 15:54:37 +0300

    Implemented CLI commands for listing, adding, and deleting recipes.

--- recipe_manager/cli.py
+++ recipe_manager/cli.py
@@ -4,8 +4,10 @@ import click
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+# Import your SQLAlchemy models here
+from recipe_manager.models import User, Recipe, Ingredient
 
-# Database connection string (SQLite in this example)
+# Define your DATABASE_URL here
 DATABASE_URL = 'sqlite:///recipe_manager.db'
 
 engine = create_engine(DATABASE_URL)
@@ -14,13 +16,65 @@ Session = sessionmaker(bind=engine)
 @click.command()
 def list_recipes():
     """List all recipes."""
-    # Create a database session
     session = Session()
+    
+    # Fetch all recipes from the database
+    recipes = session.query(Recipe).all()
+    
+    if recipes:
+        click.echo("Recipes:")
+        for recipe in recipes:
+            click.echo(f"- {recipe.title}")
+    else:
+        click.echo("No recipes found.")
+    
+    session.close()
 
-    # Implement the logic to fetch and display recipes here
+@click.command()
+@click.option('--title', prompt='Title of the recipe', help='Title of the recipe')
+@click.option('--instructions', prompt='Recipe instructions', help='Instructions for the recipe')
+@click.option('--ingredients', prompt='Recipe ingredients (comma-separated)', help='Ingredients for the recipe')
+def add_recipe(title, instructions, ingredients):
+    """Add a new recipe."""
+    session = Session()
+    
+    # Split ingredients into a list
+    ingredient_list = [name.strip() for name in ingredients.split(',')]
+    
+    # Create a new recipe and add it to the database
+    new_recipe = Recipe(title=title, instructions=instructions)
+    
+    # Fetch or create ingredients and add them to the recipe
+    for ingredient_name in ingredient_list:
+        ingredient = session.query(Ingredient).filter_by(name=ingredient_name).first()
+        if not ingredient:
+            ingredient = Ingredient(name=ingredient_name)
+        new_recipe.ingredients.append(ingredient)
+    
+    session.add(new_recipe)
+    session.commit()
+    session.close()
 
-    # Remember to close the session
+@click.command()
+@click.option('--recipe_id', prompt='Recipe ID to delete', help='ID of the recipe to delete')
+def delete_recipe(recipe_id):
+    """Delete a recipe by its ID."""
+    session = Session()
+    
+    # Find the recipe by ID and delete it
+    recipe = session.query(Recipe).filter_by(id=recipe_id).first()
+    
+    if recipe:
+        session.delete(recipe)
+        session.commit()
+        click.echo(f"Recipe with ID {recipe_id} deleted.")
+    else:
+        click.echo(f"No recipe found with ID {recipe_id}.")
+    
     session.close()
 
 if __name__ == '__main__':
+    # Add the commands to the CLI interface
+    add_recipe()
     list_recipes()
+    delete_recipe()
