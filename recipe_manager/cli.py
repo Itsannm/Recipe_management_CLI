a2a163dc0dd2861b89fed82db7505fc4e5280429
Itsannm <ann.mwanzia@student.moringaschool.com>
2023-09-05 15:49:33 +0300

    Implemented a basic CLI command to add recipes.Added database connection and session management to CLI commands.

--- recipe_manager/cli.py
+++ recipe_manager/cli.py
@@ -1,14 +1,26 @@
 # recipe_manager/cli.py
 
 import click
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+# Database connection string (SQLite in this example)
+DATABASE_URL = 'sqlite:///recipe_manager.db'
+
+engine = create_engine(DATABASE_URL)
+Session = sessionmaker(bind=engine)
 
 @click.command()
-@click.option('--title', prompt='Title of the recipe', help='Title of the recipe')
-@click.option('--instructions', prompt='Recipe instructions', help='Instructions for the recipe')
-def add_recipe(title, instructions):
-    """Add a new recipe."""
-    # Implement the logic to add a recipe to the database here
-    pass
+def list_recipes():
+    """List all recipes."""
+    # Create a database session
+    session = Session()
+
+    # Implement the logic to fetch and display recipes here
+
+    # Remember to close the session
+    session.close()
 
 if __name__ == '__main__':
-    add_recipe()
+    list_recipes()
